2ad523389a1bfac464d2f6fc908794d2d2960fbc
Tushar040903 <tusharbhardwaj9873010398@gmail.com>
2023-10-03 12:03:12 +0530

    provide the code

---
+++ app/src/main/java/com/example/calculator1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.calculator1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnAdd.setOn
+
+    }
+}
